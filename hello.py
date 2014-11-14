4b4cd939008b8f86020bec58228c46f3c8de47d9
Sting <Sting@Pc-17.local>
2014-11-13 18:08:52 -0600

    First Python/Flask server

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "Index Page"
+
+@app.route("/hello")
+def hello():
+	return "Hello World"
+
+@app.route('/users/<username>')
+def show_user_profile(username):
+	#show the user profile for that user
+	return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	#show the post
+	return 'Post %d' % post_id
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
